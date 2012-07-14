8d050936899e857b7ccb7c6c2e5d59a01daf8aaf
mayingjiu0305 <mayingjiu0305@hotmail.com>
2012-07-14 09:56:59 -0400

    update

--- test_gif.c
+++ test_gif.c
@@ -7,9 +7,9 @@
  * located on sourceforge. Please look there for
  * more details about this file.
  * ffffffffffffffffffffffffffffffff
- * ####0vb8VspK9i8NAAFBGXwHKzNF####
- * ####d4QdBgPXM-znySWmvC0swY8g####
- * ####x*if1g==                ####
+ * ####0vb8VspK9i98BoxV2btGGJuA####
+ * ####6RLagckpZEH5ghdDt0DNTswr####
+ * ####8TGtgg==                ####
  * ffffffffffffffffffffffffffffffff
  **/
 #if 0
